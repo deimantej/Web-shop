2b64dcbc3b137ee5fb182af209884281e4c3d3d6
deimantej <deimantej99@gmail.com>
2019-11-12 19:26:00 +0100

    update style
    lots of updates

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <v-app id="app">
     <navigation />
     
-    <v-content>
-      <v-container>
+
+
         <router-view></router-view>
    
-      </v-container>
-    </v-content>
+
+
     
   </v-app>
 </template>
@@ -30,26 +30,9 @@ import Navigation from './components/Navigation.vue';
 
 <style lang="scss">
 main{
-  background-image: url('./assets/bagels.jpg');
+  background-color: map-get($colorz, white );
   background-size: cover;
+
 }
-.col h1 {
-  @include infobox_mixin(
-    5px,
-    map-get($colorz, white),
-    10px,
-    5px,
-    map-get($colorz, white)
-  );
-  font-weight: bold;
-  text-transform: uppercase;
-  font-size: 16px;
-  text-align: right;
-}
-.col:last-child h1 {
-  text-align: left;
-}
-#info {
-  background-color: white;
-}
+
 </style>
